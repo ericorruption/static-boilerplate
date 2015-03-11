8a7a758527b7dd88b3b088cb1e9533e3e159a637
Eric Quanz <eric.quanz@gmail.com>
2015-03-11 14:05:53 -0300

    track javascript errors on ga

--- js/_plugins.js
+++ js/_plugins.js
@@ -1,1 +1,21 @@
 // You can put related scripts in here
+
+// Track JavaScript errors in Google Analytics
+(function(window){
+    var undefined,
+        link = function (href) {
+            var a = window.document.createElement('a');
+            a.href = href;
+            return a;
+        };
+    window.onerror = function (message, file, line, column) {
+        var host = link(file).hostname;
+        ga('send', {
+          'hitType': 'event',
+          'eventCategory': (host == window.location.hostname || host == undefined || host == '' ? '' : 'external ') + 'error',
+          'eventAction': message,
+          'eventLabel': (file + ' LINE: ' + line + (column ? ' COLUMN: ' + column : '')).trim(),
+          'nonInteraction': 1
+        });
+    };
+}(window));
