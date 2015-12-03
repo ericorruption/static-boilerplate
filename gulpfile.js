ace3449cc493090e37a35ec2c76103fe0c32a622
Eric Quanz <eric.quanz@gmail.com>
2015-12-03 13:35:42 -0200

    further adhere to a coding style

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,42 @@ var onError = function(err) {
 };
 
 gulp.task('javascript', function() {
-    webpack({
-        entry: "./src/js/main.js",
-        output: {
-            filename: "./dist/js/bundle.js"
-        },
-        plugins: [
-            new webpack.optimize.UglifyJsPlugin({ output: { comments: false }})
-        ]
-    }, function () {
-        browserSync.reload();
-    });
+  webpack({
+    entry: "./src/js/main.js",
+    output: {
+      filename: "./dist/js/bundle.js"
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({ output: { comments: false }})
+    ]
+  }, function () {
+    browserSync.reload();
+  });
 });
 
 gulp.task('css', function() {
-    return gulp.src(['src/scss/*.scss', '!src/scss/_*.scss'])
-        .pipe(plugins.plumber({ errorHandler: onError }))
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.sass())
-        .pipe(plugins.postcss([
-            require('autoprefixer'),
-            require('cssgrace'),
-            require('postcss-import'),
-        ]))
-        .pipe(plugins.minifyCss())
-        .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.stream());
+  return gulp.src(['src/scss/*.scss', '!src/scss/_*.scss'])
+    .pipe(plugins.plumber({ errorHandler: onError }))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.postcss([
+      require('autoprefixer'),
+      require('cssgrace'),
+      require('postcss-import')
+    ]))
+    .pipe(plugins.minifyCss())
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('images', function() {
-    return gulp.src('src/img/**/*')
-        .pipe(plugins.imagemin({
-          progressive: true,
-          interlaced: true
-        }))
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('src/img/**/*')
+    .pipe(plugins.imagemin({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('dist/img'));
 });
 
 // SPRITE SUPPORT WITH gulp.spritesmith
@@ -79,18 +79,18 @@ gulp.task('html', function() {
 // });
 
 gulp.task('serve', ['build'], function() {
-    browserSync.init({
-        // using vagrant or other server:
-        // proxy: "http://localhost:8000"
-        // or use builtin server:
-        server: './dist',
-        files: ['src/*.html']
-    });
+  browserSync.init({
+    // using vagrant or other server:
+    // proxy: "http://localhost:8000"
+    // or use builtin server:
+    server: './dist',
+    files: ['src/*.html']
+  });
 
-    gulp.watch(['src/*.html'], ['html']);
-    gulp.watch(['src/img/**/*'], ['images']);
-    gulp.watch(['src/scss/**/*.scss'], ['css']);
-    gulp.watch(['src/js/**/*.js'], ['javascript']);
+  gulp.watch(['src/*.html'], ['html']);
+  gulp.watch(['src/img/**/*'], ['images']);
+  gulp.watch(['src/scss/**/*.scss'], ['css']);
+  gulp.watch(['src/js/**/*.js'], ['javascript']);
 });
 
 gulp.task('build', ['html', 'css', 'javascript', 'images']);
