c907e96eccb7dd4d3447e43a6ce6520035364c91
Eric Quanz <eric.quanz@gmail.com>
2015-12-03 17:42:19 -0200

    use editorconfig defaults on more files

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ var FastClick = require('fastclick');
 require('./track-js-errors');
 
 if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
-    }, false);
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
 }
 
 (function ($) {
-    'use strict';
-    // do shit
+  'use strict';
+  // do shit
 } (jQuery));
--- src/js/track-js-errors.js
+++ src/js/track-js-errors.js
@@ -1,19 +1,20 @@
 // Track JavaScript errors in Google Analytics
 module.exports = (function(window){
-    var undefined,
-        link = function (href) {
-            var a = window.document.createElement('a');
-            a.href = href;
-            return a;
-        };
-    window.onerror = function (message, file, line, column) {
-        var host = link(file).hostname;
-        ga('send', {
-          'hitType': 'event',
-          'eventCategory': (host == window.location.hostname || host == undefined || host == '' ? '' : 'external ') + 'error',
-          'eventAction': message,
-          'eventLabel': (file + ' LINE: ' + line + (column ? ' COLUMN: ' + column : '')).trim(),
-          'nonInteraction': 1
-        });
-    };
+  var undefined,
+      link = function (href) {
+        var a = window.document.createElement('a');
+        a.href = href;
+        return a;
+      };
+
+  window.onerror = function (message, file, line, column) {
+    var host = link(file).hostname;
+    ga('send', {
+      'hitType': 'event',
+      'eventCategory': (host == window.location.hostname || host == undefined || host == '' ? '' : 'external ') + 'error',
+      'eventAction': message,
+      'eventLabel': (file + ' LINE: ' + line + (column ? ' COLUMN: ' + column : '')).trim(),
+      'nonInteraction': 1
+    });
+  };
 }(window));
