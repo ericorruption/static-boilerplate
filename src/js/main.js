17cc84c19faae2d95f75c821ab463958a0d5e480
Eric Quanz <eric.quanz@gmail.com>
2016-01-20 11:45:07 -0200

    remove fastclick
    with the ios 9 improvements to the tap delay, the hassle of loading one more library to improve the experience of old-browser users is not worth it.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
-var FastClick = require('fastclick');
 require('./track-js-errors');
-
-if ('addEventListener' in document) {
-  document.addEventListener('DOMContentLoaded', function() {
-    FastClick.attach(document.body);
-  }, false);
-}
 
 (function ($) {
   'use strict';
