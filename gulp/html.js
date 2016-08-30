f18af15df2e88634379efa244d0ec6d121600e0c
Eric Quanz <eric.quanz@gmail.com>
2016-08-30 14:28:12 -0300

    remove gulp-html5-lint because it takes too much time and only works with full html files or streams

--- gulp/html.js
+++ gulp/html.js
@@ -3,16 +3,10 @@
 import gulp        from 'gulp';
 import browserSync from 'browser-sync';
 import minifyHtml  from 'gulp-htmlmin';
-import validator   from 'gulp-html5-lint';
 
 browserSync.get('static-boilerplate');
 
-gulp.task('validate-html', () =>
-  gulp.src('src/*.html')
-  .pipe(validator())
-);
-
-gulp.task('html', ['validate-html'], () =>
+gulp.task('html', () =>
   gulp.src('src/*.html')
     .pipe(minifyHtml({
       collapseWhitespace: true
